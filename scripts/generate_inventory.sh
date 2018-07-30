47f0718c159b7eb755000ff8584721cc3109f5f3
Guy Fankam <guy.b.fankam@accenture.com>
2018-07-30 15:04:09 -0400

    First commit

---
+++ scripts/generate_inventory.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## VARIABLES
+# Path of current script directory
+PARENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+# Path of root Git directory - assuming it's one level above current script directory
+HOME_DIR="$(dirname "$PARENT_DIR")"
+# Path of ansible inventory file
+ANSIBLE_DIR="$HOME_DIR/ansible_inventory"
+# Path of default ansible directory
+ANSIBLE_DEFAULT_DIR="/etc/ansible/hosts"
+# GUID
+GUID="hostname|awk -F. '{print $2}'"
+
+echo "Copying inventory file to /etc/ansible/hosts..."
+cp ANSIBLE_DIR ANSIBLE_DEFAULT_DIR
+
+echo "Setting GUID in inventory file..."
+sed -i "s/GUID/$GUID/g" /etc/ansible/hosts
