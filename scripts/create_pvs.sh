b26f483ffc6dc1c936bfd77ab82cf99d163753db
Guy Fankam <guy.b.fankam@accenture.com>
2018-07-31 15:09:36 -0400

    Fixed selector format

---
+++ scripts/create_pvs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Creating PV directories..."
+mkdir -p /srv/nfs/user-vols/pv{1..200}
+
+echo "Creating PVs..."
+for pvnum in {1..50} ; do
+echo /srv/nfs/user-vols/pv${pvnum} *(rw,root_squash) >> /etc/exports.d/openshift-uservols.exports
+chown -R nfsnobody.nfsnobody  /srv/nfs
+chmod -R 777 /srv/nfs
+done
